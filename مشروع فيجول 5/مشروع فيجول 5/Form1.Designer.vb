25f87a3d23fdc181864bfbad418a9584f9fe8a53
Alwfy-Sadq <kkvklvovpvk@gmail.com>
2025-10-28 18:46:30 +0300

    Add files via upload

---
+++ مشروع فيجول 5/مشروع فيجول 5/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ClockToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnDate = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnTime = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblExit = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClockToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(298, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ClockToolStripMenuItem
+        '
+        Me.ClockToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnDate, Me.btnTime})
+        Me.ClockToolStripMenuItem.Name = "ClockToolStripMenuItem"
+        Me.ClockToolStripMenuItem.Size = New System.Drawing.Size(57, 24)
+        Me.ClockToolStripMenuItem.Text = "Clock"
+        '
+        'btnDate
+        '
+        Me.btnDate.Name = "btnDate"
+        Me.btnDate.Size = New System.Drawing.Size(181, 26)
+        Me.btnDate.Text = "Date"
+        '
+        'btnTime
+        '
+        Me.btnTime.Name = "btnTime"
+        Me.btnTime.Size = New System.Drawing.Size(181, 26)
+        Me.btnTime.Text = "Time"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AcceptsReturn = True
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(72, 81)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(155, 24)
+        Me.TextBox1.TabIndex = 1
+        '
+        'lblExit
+        '
+        Me.lblExit.BackColor = System.Drawing.Color.White
+        Me.lblExit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.lblExit.ForeColor = System.Drawing.Color.Red
+        Me.lblExit.Location = New System.Drawing.Point(119, 126)
+        Me.lblExit.Name = "lblExit"
+        Me.lblExit.Size = New System.Drawing.Size(68, 29)
+        Me.lblExit.TabIndex = 2
+        Me.lblExit.Text = "خروج"
+        Me.lblExit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(298, 367)
+        Me.Controls.Add(Me.lblExit)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ClockToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents btnDate As ToolStripMenuItem
+    Friend WithEvents btnTime As ToolStripMenuItem
+    Friend WithEvents lblExit As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ مشروع فيجول 5/مشروع فيجول 5/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    ' زر عرض الوقت
+    Private Sub btnTime_Click(sender As Object, e As EventArgs) Handles btnTime.Click
+        TextBox1.Text = DateTime.Now.ToString("HH:mm:ss")
+    End Sub
+
+    ' زر عرض التاريخ
+    Private Sub btnDate_Click(sender As Object, e As EventArgs) Handles btnDate.Click
+        TextBox1.Text = Date.Today.ToString("dd/MM/yyyy")
+    End Sub
+
+    ' زر أو ليبل الخروج
+    Private Sub lblExit_Click(sender As Object, e As EventArgs) Handles lblExit.Click
+        End
+    End Sub
+
+End Class
